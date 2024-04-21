07c5ac06207c4f11f5123bf1629a14d22ba099f5
Rory <rorypendragon7@gmail>
2024-04-21 02:14:37 -0300

    commit 07.js

--- C5 Fundamentos Javascript/ejercicios/07.js
+++ C5 Fundamentos Javascript/ejercicios/07.js
@@ -3,7 +3,7 @@ function esTipoDato(valor) {
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
   // Tu código:
-  return Object;
+  let esTipoDato = typeof valor;
+  return esTipoDato;
 }
-
 module.exports = esTipoDato;
