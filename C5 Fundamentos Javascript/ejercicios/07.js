fc30f92f1e8653f94f27ff049d63f90f56b8b0d0
Rory <rorypendragon7@gmail>
2024-04-21 02:15:43 -0300

    commit 07.js

--- C5 Fundamentos Javascript/ejercicios/07.js
+++ C5 Fundamentos Javascript/ejercicios/07.js
@@ -3,6 +3,8 @@ function esTipoDato(valor) {
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
   // Tu código:
+  /* la razon porque este resultado es correcto es debido a la llamda a "VALOR" es debido a que es una varible y 
+  typeof retorna todos los posibles valores de una variable pero en formma de string */
   let esTipoDato = typeof valor;
   return esTipoDato;
 }
